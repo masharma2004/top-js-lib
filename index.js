9f966dfa6da533c9a6f2e1effe8a501ecc69230a
Mayank Sharma <36720230+masharma2004@users.noreply.github.com>
2024-07-16 22:09:13 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+let myLibrary=[];   
+
+function Book(title,author,pages,read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read; 
+
+}
+
+Book.prototype.toggleRead=function(){
+    this.read=!this.read;
+}
+
+function toggleRead(index){
+    myLibrary[index].toggleRead();
+    render();
+}
+
+function render(){
+    let libraryEl=document.querySelector("#library");
+    libraryEl.innerHTML="";
+    for(let i=0;i<myLibrary.length;i++){
+        let book=myLibrary[i];
+        let bookEl=document.createElement("div");
+        bookEl.setAttribute("class","book-card");
+        bookEl.innerHTML=`<div class="card-header">
+        <h3 class="title">${book.title}</h3>
+        <h5 class="author">${book.author}</h5>
+    </div>
+    <div class="card-body">
+        <p class="pages">${book.pages} pages</p>
+        <p class="read">${book.read?"Read":"Not Read"}</p>
+        <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+    </div>`;
+        libraryEl.appendChild(bookEl);
+
+        
+    }
+}
+    function removeBook(index){
+        myLibrary.splice(index,1);
+        render();
+    
+    }
+
+function addBookToLibrary(){
+    let title=document.querySelector("#title").value;
+    let author=document.querySelector("#author").value;
+    let pages=document.querySelector("#pages").value;
+    let read=document.querySelector("#read").checked;
+    let newBook=new Book(title,author,pages,read);
+    const duplicate = myLibrary.some(book => book.title === title && book.author === author);
+    if (duplicate) {
+        alert("This book already exists in your library.");
+        return; 
+    }
+    myLibrary.push(newBook);
+    render();
+
+}
+
+let newBookbtn=document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click",()=>{
+    let newBookForm=document.querySelector("#new-book-form");
+    newBookForm.style.display="block";
+
+});
+
+document.querySelector("#submit").addEventListener("click",(event)=>{
+    event.preventDefault();
+    addBookToLibrary();
+});
